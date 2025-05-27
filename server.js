ece877607b8938c77a2538f4c330f9baafcbf6ea
priya678 <priya77reju@gmail.com>
2025-05-27 17:47:38 +0000

    deleted files

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req,res){
-	        res.writeHead(200, { 'Content-Type': 'text/plain' });
-	        res.end('Hello World!');
-}).listen(3333);
-console.log('Server started on localhost:3333; press Ctrl-C to terminate...!');
